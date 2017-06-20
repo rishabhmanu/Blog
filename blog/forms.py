cd145d76fae4c54ee6192036dc8b78ca546b64bf
rishabhmanu <rishabhdevmanu@gmail.com>
2017-06-20 16:23:05 +0530

    Comment function for users -- patches

--- blog/forms.py
+++ blog/forms.py
@@ -1,7 +1,12 @@
 from django import forms
-from .models import Post
+from .models import Post, Comment
 # our new form
 class ContactForm(forms.Form):
     from_email = forms.EmailField(label='Your Email', required=True)
     subject = forms.CharField(required=True)
     message = forms.CharField(widget=forms.Textarea, required=True)
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ('author', 'text',)
--- blog/views.py
+++ blog/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 from .models import Post, Comment
 from django.utils import timezone
-from .forms import ContactForm
+from .forms import ContactForm, CommentForm
 from django.core.mail import send_mail, BadHeaderError
+from django.contrib.auth.decorators import login_required
 
 def index(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
@@ -35,3 +36,28 @@ def contact(request):
 
 def success(request):
     return render(request, "blog/success.html", {})
+
+def add_comment_to_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect('post', pk=post.pk)
+    else:
+        form = CommentForm()
+    return render(request, "blog/add_comment_to_post.html", {'form': form})
+
+@login_required
+def comment_approve(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    comment.approve()
+    return redirect('post', pk=comment.post.pk)
+
+@login_required
+def comment_remove(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    comment.delete()
+    return redirect('post', pk=comment.post.pk)
