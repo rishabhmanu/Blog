1d914489c7ab4cdbf44f1b724751ddd644607118
rishabhmanu <rishabhdevmanu@gmail.com>
2017-06-20 16:46:32 +0530

    some template modifications

--- blog/forms.py
+++ blog/forms.py
@@ -3,7 +3,7 @@ from .models import Post, Comment
 # our new form
 class ContactForm(forms.Form):
     from_email = forms.EmailField(label='Your Email', required=True)
-    subject = forms.CharField(required=True)
+    name = forms.CharField(label='Your Name',required=True)
     message = forms.CharField(widget=forms.Textarea, required=True)
 
 class CommentForm(forms.ModelForm):
--- blog/views.py
+++ blog/views.py
@@ -24,11 +24,11 @@ def contact(request):
     else:
         form = ContactForm(request.POST)
         if form.is_valid():
-            subject = form.cleaned_data['subject']
+            name = form.cleaned_data['name']
             from_email = form.cleaned_data['from_email']
             message = form.cleaned_data['message']
             try:
-                send_mail(subject, message, from_email, ['admin@example.com'])
+                send_mail(name, message, from_email, ['admin@example.com'])
             except BadHeaderError:
                 return HttpResponse('Invalid header found.')
             return redirect('success')
